cb0a1fa837cc6795b0b936975382c9b3541fa72f
Tanaka <f.tanaka@gftd.works>
2022-08-16 15:39:25 +0900

    async/await Promiseについての復習

--- index.js
+++ index.js
@@ -21,27 +21,50 @@ function getC() {
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
 
-//参考：async/await文で記述した場合
-//値を取得し、それらの積を返す関数を定義
-/* async function main() {
-  //個々の値を取得
+/* 
+  Promiseの結果をコールバック関数に渡す際には、thenを挟む必要がある。
+  async関数を実行してreturnされた値を、別の関数で使用する際も、thenで挟む必要がある。
+*/
+
+//async/await文で記述した場合の例１
+async function main() {
   const a = await getA();
   const b = await getB();
   const c = await getC();
-
-  //取得した値の積をreturn
-  return a * b * c;
 }
-//解決されたPromiseを受け取り、thenでコールバック関数に渡す
-main().then((num) => {
-  console.log(num);  //受け取った値を表示
-}); */
+main().then(num => {  //(num)とせずに、numとするだけでも問題ないらしい
+  console.log(num);
+});
 
+//async/await文で記述した場合の例２(例１をコメントアウトしないと、undefinedになる)
+getA().then(async a => {  
+  const b = await getB();
+  const c = await getC();
+  console.log(a * b * c);
+});
 
-getA().then((a) => {
-  return getB().then((b) =>{
-    return getC().then((c) => {
+//Promiseチェーンの場合の例１
+getA().then(a => {  
+  return getB().then(b =>{
+    return getC().then(c => {
       console.log(a*b*c);
     });
   });
+});
+
+//Promiseチェーンの場合の例２
+getA().then(a => {  
+    return getB().then(b => {
+      return a * b;
+    });
+  }).then(result => {
+    getC().then(c => {
+      console.log(result * c);
+    });
+});
+
+//Promise.allの場合
+//Promise.allの場合は、複数のPromiseの解決が同時に開始されるので、早く終わる
+Promise.all([getA(), getB(), getC()]).then(results => {
+  console.log(results[0] * results[1] * results[2]);
 });
